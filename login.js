cb01b76944b664a84741b304f28dd7dd6106fb22
justindudley <justin.dudley@ascendum.com>
2020-07-27 23:52:54 -0400

    remove boilerplate text, add login component

--- login.js
+++ login.js
@@ -1,15 +1,11 @@
-'use strict';
-
 // var React = require('react-native'); // Yup, this older syntax breaks it !!!
 // var Text = React.Text;
-
+'use strict';
 import React from 'react';
 import {Text} from 'react-native'; // see older syntax below
 
 export class Login extends React.Component {
   render() {
-    return <Text>Hello Justin!</Text>;
+    return <Text style={{color: 'green'}}>Hello Justin!</Text>;
   }
 }
-
-// module.exports = Login;
