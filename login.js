c5610adbd0573b895de10b9c5b6bd2d61a45a9a7
justindudley <justin.dudley@ascendum.com>
2020-08-20 16:34:05 -0400

    add two TextInputs, and Text box saying 'Log in'

--- login.js
+++ login.js
@@ -2,7 +2,14 @@
 // var Text = React.Text;
 'use strict';
 import React, {Fragment} from 'react';
-import {Text, Image, View, StyleSheet} from 'react-native'; // see older syntax below
+import {
+  Text,
+  Image,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableHighlight,
+} from 'react-native'; // see older syntax below
 
 export class Login extends React.Component {
   render() {
@@ -14,6 +21,15 @@ export class Login extends React.Component {
             source={require('./myImages/octocat.png')}
           />
           <Text style={styles.heading}>Github browser</Text>
+          <TextInput style={styles.input} placeholder="Github username" />
+          <TextInput
+            style={styles.input}
+            placeholder="Github password"
+            secureTextEntry="true"
+          />
+          <TouchableHighlight style={styles.button}>
+            <Text style={styles.buttonText}>Log in</Text>
+          </TouchableHighlight>
         </View>
       </Fragment>
     );
@@ -26,6 +42,7 @@ const styles = StyleSheet.create({
     flex: 1, // so that the div takes up space
     paddingTop: 40,
     alignItems: 'center',
+    padding: 10,
   },
   logo: {
     width: 66,
@@ -34,5 +51,26 @@ const styles = StyleSheet.create({
   heading: {
     fontSize: 30,
     marginTop: 10,
+  },
+  input: {
+    height: 50,
+    marginTop: 10,
+    padding: 4,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#48bbec',
+    alignSelf: 'stretch',
+  },
+  button: {
+    height: 50,
+    backgroundColor: '#48bbec',
+    alignSelf: 'stretch',
+    marginTop: 10,
+    justifyContent: 'center',
+  },
+  buttonText: {
+    fontSize: 22,
+    color: '#fff',
+    alignSelf: 'center',
   },
 });
